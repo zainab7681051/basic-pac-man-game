4b763d40636a0e0247e0f41cadeb8e450aa66749
zainab7681051 <zainab7681051@gmail.com>
2022-12-25 20:49:32 +0300

    initial stages

--- main.js
+++ main.js
@@ -1,10 +1,134 @@
+//minute==>45:36
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 canvas.width = innerWidth - 300
 canvas.height = innerHeight
 
 class Boundary {
-	constructor() {
+	static width = 40
+	static height = 40
+	constructor({
+		position
+	}) {
+		this.position = position
+		this.width = 40
+		this.height = 40
+	}
 
+	draw() {
+		c.fillStyle = 'blue'
+		c.fillRect(this.position.x,
+			this.position.y, this.width,
+			this.height)
 	}
 }
+
+class Player {
+	constructor({
+		position,
+		velocity
+	}) {
+		this.position = position
+		this.velocity = velocity
+		this.radius = 15
+	}
+
+	draw() {
+		c.beginPath()
+		c.arc(this.position.x,
+			this.position.y,
+			this.radius, 0,
+			Math.PI * 2)
+		c.fillStyle = 'yellow'
+		c.fill()
+		c.closePath()
+	}
+
+	update() {
+		this.draw()
+		this.position.x += this.velocity.x
+		this.position.y += this.velocity.y
+	}
+}
+
+const map = [
+['-', '-', '-', '-', '-', '-'],
+['-', ' ', ' ', ' ', ' ', '-'],
+['-', ' ', '-', '-', ' ', '-'],
+['-', ' ', ' ', ' ', ' ', '-'],
+['-', '-', '-', '-', '-', '-']
+]
+const boundaries = []
+const player = new Player({
+	position: {
+		x: Boundary.width + Boundary.width / 2,
+		y: Boundary.height + Boundary.height / 2
+	},
+	velocity: {
+		x: 0,
+		y: 0
+	}
+})
+
+map.forEach((row, i) => {
+	row.forEach((symbol, j) => {
+		switch (symbol) {
+		case '-':
+			boundaries.push(
+				new Boundary({
+					position: {
+						x: Boundary.width * j,
+						y: Boundary.height * i
+					}
+				})
+			)
+			break;
+		}
+	})
+})
+
+function animate() {
+	requestAnimationFrame(animate)
+	c.clearRect(0, 0, canvas.width, canvas.height)
+	boundaries.forEach((boundary) => {
+		boundary.draw()
+	})
+
+	player.update()
+}
+
+animate()
+
+addEventListener('keydown', ({
+	key
+}) => {
+	switch (key) {
+	case 'w': //up
+		player.velocity.y = -5;
+		break;
+	case 'a': //left
+		player.velocity.x = -5
+		break;
+	case 's': //down
+		player.velocity.y = 5
+	case 'd': //right
+		player.velocity.x = 5
+	}
+})
+
+addEventListener('keyup', ({
+	key
+}) => {
+	switch (key) {
+	case 'w': //up
+		player.velocity.y = 0;
+		break;
+	case 'a': //left
+		player.velocity.x = 0
+		break;
+	case 's': //down
+		player.velocity.y = 0
+	case 'd': //right
+		player.velocity.x = 0
+	}
+})
